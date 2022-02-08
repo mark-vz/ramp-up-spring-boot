4a6f5eb7edf65a3d5c776275a9b23bf39a4114f4
Mark von Zeschau <m.vonzeschau@tarent.de>
2022-02-08 19:53:16 +0100

    Remove obsolete setters

--- my-user-mgmt/src/main/java/com/example/myusermgmt/user/domain/User.java
+++ my-user-mgmt/src/main/java/com/example/myusermgmt/user/domain/User.java
@@ -3,10 +3,10 @@ package com.example.myusermgmt.user.domain;
 import java.util.UUID;
 
 public class User {
-  private UUID id;
-  private String firstName;
-  private String lastName;
-  private String emailAddress;
+  private final UUID id;
+  private final String firstName;
+  private final String lastName;
+  private final String emailAddress;
 
   public User(UUID id, String firstName, String lastName, String emailAddress) {
     this.id = id;
@@ -19,31 +19,15 @@ public class User {
     return id;
   }
 
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
   public String getFirstName() {
     return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
   }
 
   public String getLastName() {
     return lastName;
   }
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
   public String getEmailAddress() {
     return emailAddress;
-  }
-
-  public void setEmailAddress(String emailAddress) {
-    this.emailAddress = emailAddress;
   }
 }
